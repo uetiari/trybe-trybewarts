b3402b9daeff87608f04825de563f18021dd2bf6
Eliana Nunes <elianadnunes@gmail.com>
2021-07-22 11:16:38 -0300

    Requisitos 17 e 18

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-const button = document.getElementById('button');
+const loginButton = document.getElementById('button');
 const inputLogin = document.getElementById('login');
 const inputPassword = document.getElementById('password');
 
 function alertButton(event) {
-  if (inputLogin.value !== 'tryber@teste.com' && inputPassword.value !== '123456') {
+  if (
+    inputLogin.value !== 'tryber@teste.com' && inputPassword.value !== '123456') {
     alert('Login ou senha inválidos.');
   } else {
     alert('Olá, Tryber!');
   }
   return event;
 }
+loginButton.addEventListener('click', alertButton);
 
-button.addEventListener('click', alertButton);
+const submitButton = document.getElementById('submit-btn');
+const checkbox = document.getElementById('agreement');
+
+function checkButton() {
+  if (checkbox.checked) {
+    submitButton.disabled = false;
+  } else {
+    submitButton.disabled = true;
+  }
+}
+checkbox.addEventListener('click', checkButton);
+
+// Referência: https://github.com/tryber/sd-014-b-project-trybewarts/pull/45
+// [Raphael Pereira e Fabricio Cardoso]
